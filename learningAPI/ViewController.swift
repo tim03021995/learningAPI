d15c1b48c49412e5beea2c9daba0221161488640
Alvin Tseng <tim960315@gmail.com>
2020-08-17 11:06:58 +0800

    Initial Commit

---
+++ learningAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learningAPI
+//
+//  Created by Alvin Tseng on 2020/8/17.
+//  Copyright © 2020 Alvin Tseng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
